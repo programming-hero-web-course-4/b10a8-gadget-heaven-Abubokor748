c1ecb8a0584cd0e75b50a39f85efac9acc59c1c1
abubokor748 <abubokor748@gmail.com>
2024-11-06 22:36:02 +0600

    tried

--- src/componets/Categories/Categories.jsx
+++ src/componets/Categories/Categories.jsx
@@ -3,31 +3,37 @@ import { NavLink } from "react-router-dom";
 const Categories = ({ categories }) => {
   return (
     <div className="lg:col-span-3 flex justify-center mt-5">
-      <div
-        role="tablist"
-        className="tabs tabs-boxed justify-items-center flex flex-col gap-y-8 mt-5 bg-white "
-      >
-        {categories.map((category) => (
-          <NavLink
-            to={`/category/${category.category}`}
-            role="tab"
-            key={category.category}
-            className="flex justify-center"
-          >
-            {({ isActive }) => (
-              <button
-                className={`btn rounded-3xl w-[192px] items-center text-center md:px-5 bg-base-200 hover:bg-[#9538E2] px-6 border-[#9538E2] hover:text-white ${
-                  isActive
-                    ? "bg-[#a54bee] text-white"
-                    : "bg-base-200 text-black"
-                }`}
-              >
-                {category.name}
-              </button>
-            )}
-          </NavLink>
-        ))}
-      </div>
+      {categories && categories.length > 0 ? (
+        <div
+          role="tablist"
+          className="tabs tabs-boxed justify-items-center flex flex-col gap-y-8 mt-5 bg-white"
+        >
+          {categories.map((category) => (
+            <NavLink
+              to={`/category/${category.category}`}
+              role="tab"
+              key={category.category}
+              className="flex justify-center"
+            >
+              {({ isActive }) => (
+                <button
+                  className={`btn rounded-3xl w-[192px] items-center text-center md:px-5 bg-base-200 hover:bg-[#9538E2] px-6 border-[#9538E2] hover:text-white ${
+                    isActive
+                      ? "bg-[#8940c4] text-white"
+                      : "bg-base-200 text-black"
+                  }`}
+                >
+                  {category.name}
+                </button>
+              )}
+            </NavLink>
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center mt-5">
+          <p className="text-center text-gray-500">No categories available.</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,28 +1,35 @@
 const Footer = () => {
   return (
-    <div className="p-2">
-      <footer className="footer border text-black p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-7">
-        <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-          <h6 className="footer-title">Legal</h6>
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
-        </nav>
-      </footer>
+    <div className="mt-16 bg-base-200">
+      <div className="text-center my-6 pt-6">
+        <h2 className="font-bold text-2xl">Gadget Heaven</h2>
+        <p>Leading the way in cutting-edge Technology and Innovation</p>
+      </div>
+      <hr />
+      <div className="p-2">
+        <footer className="footer text-black p-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-7">
+          <nav>
+            <h6 className="footer-title">Services</h6>
+            <a className="link link-hover">Branding</a>
+            <a className="link link-hover">Design</a>
+            <a className="link link-hover">Marketing</a>
+            <a className="link link-hover">Advertisement</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Company</h6>
+            <a className="link link-hover">About us</a>
+            <a className="link link-hover">Contact</a>
+            <a className="link link-hover">Jobs</a>
+            <a className="link link-hover">Press kit</a>
+          </nav>
+          <nav>
+            <h6 className="footer-title">Legal</h6>
+            <a className="link link-hover">Terms of use</a>
+            <a className="link link-hover">Privacy policy</a>
+            <a className="link link-hover">Cookie policy</a>
+          </nav>
+        </footer>
+      </div>
     </div>
   );
 };
